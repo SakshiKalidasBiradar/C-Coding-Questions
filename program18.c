e04045fb6368aeeaa05f48d9772c1d8c62a0d7e8
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-07-06 11:37:34 +0530

    Add files via upload

---
+++ program18.c
@@ -0,0 +1,20 @@
+//write program which accept number from user and display all its non factors
+#include<stdio.h>
+void DisplayFact(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo;)
+    
+}
+
+int main()
+{
+    int iValue = 0;
+  
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    DisplayFact(iValue);
+
+    return 0;
+}
