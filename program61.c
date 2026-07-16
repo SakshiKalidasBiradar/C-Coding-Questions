539cfab8faccae81b31371f1446b211fc6e90412
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-07-16 22:02:15 +0530

    Add files via upload

---
+++ program61.c
@@ -0,0 +1,55 @@
+//Accept N Numbers from user and return difference Between summation of even Elements and Summation of Odd Elements
+#include<stdio.h>
+#include<stdlib.h>
+
+int Difference(int Arr[],int iLength)
+{
+    int iEvenSum = 0,iOddSum = 0;
+    int iCnt = 0;
+    for(iCnt=0;iCnt<iLength;iCnt++)
+    {
+        if(Arr[iCnt]%2 == 0)
+        {
+            iEvenSum = iEvenSum+Arr[iCnt];
+        }
+        else
+        {
+            iOddSum=iOddSum+Arr[iCnt];
+        }
+        
+    }
+    return (iEvenSum-iOddSum);
+ 
+    
+}
+#include<stdio.h>
+int main()
+{
+    int iSize = 0,iRet = 0,iCnt = 0;
+    int*p=NULL;
+
+    printf("Enter Number of elements:\n");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize*sizeof(int));
+    if(p==NULL)
+    {
+        printf("Unable to Allocate Memory");
+        return -1;
+    }
+    printf("Enter %d Elements\n",iSize);
+    for(iCnt = 0;iCnt<iSize;iCnt++)
+    {
+        printf("Enter element : %d\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Difference(p,iSize);
+    printf("result is %d",iRet);
+
+    free(p);
+    return 0;
+
+
+
+}
